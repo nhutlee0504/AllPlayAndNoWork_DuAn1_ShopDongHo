ba282460a0a640ed5595ae8096aea60216a2726c
nhutlee0405 <leminhnhut718@gmai.com>
2023-11-18 08:10:51 +0700

    New Git

---
+++ QuanLyDongHo/QuanLyDongHo/Forms/DonHang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyDongHo.Forms
+{
+    public partial class DonHang : Form
+    {
+        public DonHang()
+        {
+            InitializeComponent();
+        }
+    }
+}
